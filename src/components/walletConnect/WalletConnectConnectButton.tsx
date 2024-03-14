3e2b2570f5ad5f6e86238a9fe2c2bfb47ec7f518
rabbitblood <qz154772610@gmail.com>
2024-03-13 17:30:34 -0700

    update UI

--- src/components/walletConnect/WalletConnectConnectButton.tsx
+++ src/components/walletConnect/WalletConnectConnectButton.tsx
@@ -1,17 +1,50 @@
 import { useWeb3Modal } from "@web3modal/wagmi/react";
 import { useAccount } from "wagmi";
 import GeneralButton from "../atoms/GeneralButton/GeneralButton";
+import walletIcon from "@/assets/wallet-icon.jpg";
 
 export default function WalletConnectButton() {
   const { open } = useWeb3Modal();
+  const { address } = useAccount();
   const { isConnected, isConnecting } = useAccount();
 
   const isConnectingDisplay = <div>Connecting...</div>;
 
-  const isConnectedDisplay = <div>Wallet</div>;
+  const isConnectedDisplay = (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        style={{
+          width: "1.5rem",
+          height: "1.5rem",
+          marginRight: "0.5rem",
+          borderRadius: "50%",
+        }}
+        src={walletIcon}
+        alt=""
+      />
+      {address.toString().substring(0, 3)}...
+      {address
+        .toString()
+        .substring(address.toString().length - 3, address.toString().length)}
+    </div>
+  );
 
   return (
-    <GeneralButton onClick={() => open()}>
+    <GeneralButton
+      onClick={() => open()}
+      style={{
+        padding: "0.5rem 1rem",
+        fontSize: "1rem",
+        borderWidth: "0.2rem",
+        borderRadius: "1rem",
+      }}
+    >
       {isConnecting
         ? isConnectingDisplay
         : isConnected
