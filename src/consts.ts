2d15d337b2f3394b55fd0833d471eb4f2611b6a1
Shanni <shan.liu03@gmail.com>
2024-03-19 17:21:18 -0700

    Revert "edit"
    This reverts commit 5a0d89af7e80066340bf67ff20461f76ee5ca416.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-const arbitrumSepoliaContractAddress = "0x475C694baca169375Eaf00f8EE872A6459D5eA55";
-  // "0xEC752580aD907a471f0c2cD95A89840Bf3802Ade";
+const arbitrumSepoliaContractAddress =
+  "0xEC752580aD907a471f0c2cD95A89840Bf3802Ade";
 const BeraTestnetContractAddress = "0x53B9fC0A0833f2A090bB92D4914430eEd4C0AAAC";
 const acceptChainId = [421614, 80085]; //arbitrum sepolia, berachain testnet
 const maxMintAmount = 20;
@@ -10,7 +10,6 @@ const contracts = {
 
 export {
   arbitrumSepoliaContractAddress,
-  BeraTestnetContractAddress,
   acceptChainId,
   maxMintAmount,
   contracts,
