0d0775e8b348436f285d0612f29ffae575252644
rabbitblood <qz154772610@gmail.com>
2024-03-19 19:11:14 -0700

    fix time not showing on mobile device

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import "@/css/home.css";
 import MainContentWrapper from "./components/template/MainContentWrapper/MainContentWrapper";
 import GeneralButton from "./components/atoms/GeneralButton/GeneralButton";
 import { Link } from "react-router-dom";
-import Container from "./components/atoms/Container/Container";
 
 import mole from "@/assets/smoking-mole.svg";
 
 import Countdown from "@/components/molecules/CountDown/CountDown";
 
 function App() {
-  const countDownDate = new Date("2024-03-21 15:30 UTC");
+  const countDownDate = new Date(1711035000000); //"2024-03-21 15:30 UTC"
   const completeRenderer = (
     <Link to={"./mint"}>
       <GeneralButton
@@ -36,7 +35,6 @@ function App() {
             Join <span style={{ color: "#ECC94E" }}>HoneyGenesis NFT</span> for
             a thrilling battle with sweet rewards.
           </p>{" "}
-          <Container style={{ color: "#ECC94E" }}>Mint Live:</Container>
           <Countdown date={countDownDate} completeRenderer={completeRenderer} />
         </main>
       </MainContentWrapper>
