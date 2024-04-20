8b5263a66182af93892132c5f01a5754d0207dc7
rabbitblood <qz154772610@gmail.com>
2024-04-20 05:30:15 -0700

    nft display

--- src/Routes/NFT.tsx
+++ src/Routes/NFT.tsx
@@ -1,21 +1,84 @@
 import MainContentWrapper from "@/components/template/MainContentWrapper/MainContentWrapper";
 import GeneralDropDown from "@/components/atoms/GeneralDropDown/GeneralDropDown";
+import NFT_PARTS from "@/assets/nft";
 
 //imgs
 import bgImage from "@/assets/forest-bg.png";
-import nftV2 from "@/assets/nft-v2.jpg";
 import { useState } from "react";
 
 export default function NFT() {
-  const [bearType, setBearType] = useState("white");
-  const [face, setFace] = useState("normal");
-  const [cloth, setCloth] = useState("normal");
-  const [weapon, setWeapon] = useState("normal");
+  const [bearType, setBearType] = useState("pot");
+  const [layer0, setLayer0] = useState("0");
+  const [layer1, setLayer1] = useState("0");
+  const [layer2, setLayer2] = useState("0");
+  const [layer3, setLayer3] = useState("0");
+  const [layer4, setLayer4] = useState("0");
+  const [layer5, setLayer5] = useState("0");
+  const [layer6, setLayer6] = useState("0");
 
-  const bearOptions = ["white", "black", "brown"];
-  const faceOptions = ["normal", "smile", "angry"];
-  const clothOptions = ["normal", "armor", "ninja"];
-  const weaponOptions = ["normal", "sword", "gun"];
+  const bearOptions = ["pot", "predator"];
+  const layer0Options = [];
+  const layer1Options = [];
+  const layer2Options = [];
+  const layer3Options = [];
+  const layer4Options = [];
+  const layer5Options = [];
+  const layer6Options = [];
+  const layers = {
+    0: {
+      value: layer0,
+      setValue: setLayer0,
+      options: layer0Options,
+    },
+    1: {
+      value: layer1,
+      setValue: setLayer1,
+      options: layer1Options,
+    },
+    2: {
+      value: layer2,
+      setValue: setLayer2,
+      options: layer2Options,
+    },
+    3: {
+      value: layer3,
+      setValue: setLayer3,
+      options: layer3Options,
+    },
+    4: {
+      value: layer4,
+      setValue: setLayer4,
+      options: layer4Options,
+    },
+    5: {
+      value: layer5,
+      setValue: setLayer5,
+      options: layer5Options,
+    },
+    6: {
+      value: layer6,
+      setValue: setLayer6,
+      options: layer6Options,
+    },
+  };
+
+  for (const key in layers) {
+    const layer = layers[key];
+    for (let i = 0; i < NFT_PARTS[bearType][key].length; i++) {
+      layer.options.push(i);
+    }
+  }
+
+  function changeBearHandler(value: string) {
+    setBearType(value);
+    setLayer0("0");
+    setLayer1("0");
+    setLayer2("0");
+    setLayer3("0");
+    setLayer4("0");
+    setLayer5("0");
+    setLayer6("0");
+  }
 
   return (
     <div className="App nft">
@@ -23,7 +86,15 @@ export default function NFT() {
         <div className="nft-section">
           <div className="right-section">
             <div className="nft-img-container">
-              <img className="nft-img" src={nftV2} alt="Nft Image" />
+              {Object.values(layers).map((layer, index) => {
+                return (
+                  <img
+                    className="nft-img"
+                    src={NFT_PARTS[bearType][index][layer.value]}
+                    alt=""
+                  />
+                );
+              })}
             </div>
           </div>
           <main className="main nft">
@@ -31,31 +102,21 @@ export default function NFT() {
             <h1 className="title">Choose Your NFT</h1>
             <GeneralDropDown
               value={bearType}
-              setValue={setBearType}
+              setValue={changeBearHandler}
               unitName="Bear"
               options={bearOptions}
             ></GeneralDropDown>
 
-            <GeneralDropDown
-              value={face}
-              setValue={setFace}
-              unitName="Face"
-              options={faceOptions}
-            ></GeneralDropDown>
-
-            <GeneralDropDown
-              value={cloth}
-              setValue={setCloth}
-              unitName="Cloth"
-              options={clothOptions}
-            ></GeneralDropDown>
-
-            <GeneralDropDown
-              value={weapon}
-              setValue={setWeapon}
-              unitName="Weapon"
-              options={weaponOptions}
-            ></GeneralDropDown>
+            {Object.values(layers).map((layer, index) => {
+              return (
+                <GeneralDropDown
+                  value={layer.value}
+                  setValue={layer.setValue}
+                  unitName={"layer" + index.toString()}
+                  options={layer.options}
+                ></GeneralDropDown>
+              );
+            })}
           </main>
         </div>
       </MainContentWrapper>
---
+++ src/assets/nft/index.ts
@@ -0,0 +1,54 @@
+// bgimage for both predator and pot
+import bg_1 from "./bg/0-1.png";
+import bg_2 from "./bg/0-2.png";
+import bg_3 from "./bg/0-3.png";
+
+//pot
+import potLayer_1_1 from "./pot/1-1.png";
+import potLayer_1_2 from "./pot/1-2.png";
+import potLayer_2_1 from "./pot/2-1.png";
+import potLayer_2_2 from "./pot/2-2.png";
+import potLayer_3_1 from "./pot/3-1.png";
+import potLayer_3_2 from "./pot/3-2.png";
+import potLayer_4_1 from "./pot/4-1.png";
+import potLayer_4_2 from "./pot/4-2.png";
+import potLayer_5_1 from "./pot/5-1.png";
+import potLayer_5_2 from "./pot/5-2.png";
+import potLayer_6_1 from "./pot/6b-1.png";
+import potLayer_6_2 from "./pot/6b-2.png";
+import potLayer_6_3 from "./pot/6b-3.png";
+
+//predator
+import PredatorLayer_1_1 from "./predator/1-1.png";
+import PredatorLayer_1_2 from "./predator/1-2.png";
+import PredatorLayer_2_1 from "./predator/2-1.png";
+import PredatorLayer_3_1 from "./predator/3-1.png";
+import PredatorLayer_4_1 from "./predator/4-1.png";
+import PredatorLayer_4_2 from "./predator/4-2.png";
+import PredatorLayer_5_1 from "./predator/5-1.png";
+import PredatorLayer_5_2 from "./predator/5-2.png";
+import PredatorLayer_6_1 from "./predator/6a-1.png";
+import PredatorLayer_6_2 from "./predator/6a-2.png";
+
+const NFT_PARTS = {
+  pot: {
+    0: [bg_1, bg_2, bg_3],
+    1: [potLayer_1_1, potLayer_1_2],
+    2: [potLayer_2_1, potLayer_2_2],
+    3: [potLayer_3_1, potLayer_3_2],
+    4: [potLayer_4_1, potLayer_4_2],
+    5: [potLayer_5_1, potLayer_5_2],
+    6: [potLayer_6_1, potLayer_6_2, potLayer_6_3],
+  },
+  predator: {
+    0: [bg_1, bg_2, bg_3],
+    1: [PredatorLayer_1_1, PredatorLayer_1_2],
+    2: [PredatorLayer_2_1],
+    3: [PredatorLayer_3_1],
+    4: [PredatorLayer_4_1, PredatorLayer_4_2],
+    5: [PredatorLayer_5_1, PredatorLayer_5_2],
+    6: [PredatorLayer_6_1, PredatorLayer_6_2],
+  },
+};
+
+export default NFT_PARTS;
