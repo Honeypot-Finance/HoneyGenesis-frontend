7961f2ce4c482baf43f260e76e5889171c58708b
rabbitblood <qz154772610@gmail.com>
2024-10-12 21:50:03 -0700

    update bulk generateconfig

---
+++ src/assets/nft_sm/bulk_generating_index.ts
@@ -0,0 +1,364 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+//-------------common-------------
+import none from "./None.png";
+
+// layer 0 bgimage for both predator and pot
+//import bgGreen_0 from "./单色背景/0-绿色_sm.png";
+//import bgBlue_0 from "./单色背景/0-蓝色_sm.png";
+//import bgYellow_0 from "./单色背景/0-黄色_sm.png";
+import logoBlue_0 from "./单色背景/0-logo蓝色_sm.png";
+import logoYellow from "./单色背景/0-logo黄色_sm.png";
+
+//-------------predator-------------
+//layer 0 bgimage for predator
+import predator_farm_0 from "./掠夺阵营/0-农场围栏外_sm.png";
+import predator_jail_0 from "./掠夺阵营/0-监狱_sm.png";
+//layer 1 bear type
+import predator_polarBear_1 from "./掠夺阵营/1-北极熊_sm.png";
+import predator_brownBear_1 from "./掠夺阵营/1-棕熊_sm.png";
+import predator_pandaBear_1 from "./掠夺阵营/1-熊猫_sm.png";
+import predator_blackBear_1 from "./掠夺阵营/1-黑熊_sm.png";
+import predator_bullas_polarBear_1 from "./掠夺阵营/1-Bullas-北极熊_sm.png";
+import predator_bullas_brownBear_1 from "./掠夺阵营/1-Bullas-棕熊_sm.png";
+import predator_bullas_pandaBear_1 from "./掠夺阵营/1-Bullas-熊猫_sm.png";
+import predator_bullas_blackBear_1 from "./掠夺阵营/1-Bullas-黑熊_sm.png";
+import predator_beraborrow_polarBear_1 from "./掠夺阵营/1-BeraBorrow北极熊_sm.png";
+import predator_beraborrow_brownBear_1 from "./掠夺阵营/1-BeraBorrow棕熊_sm.png";
+import predator_beraborrow_pandaBear_1 from "./掠夺阵营/1-BeraBorrow熊猫_sm.png";
+import predator_beraborrow_blackBear_1 from "./掠夺阵营/1-BeraBorrow黑熊_sm.png";
+//layer 2 nail
+import predator_bottomNail_2 from "./掠夺阵营/2-下层爪子_sm.png";
+//layer 3 emotion
+import predator_anger_3 from "./掠夺阵营/3-愤怒_sm.png";
+import predator_normal_3 from "./掠夺阵营/3-默认_sm.png";
+import predator_beraborrow_smirk_3 from "./掠夺阵营/3-BeraBorrow揶揄_sm.png";
+import predator_areb_face_3 from "./掠夺阵营/3-Areb-face_sm.png";
+//layer 4 clothes
+import predator_purple_4 from "./掠夺阵营/4-紫衣服破_sm.png";
+import predator_green_4 from "./掠夺阵营/4-绿衣服破_sm.png";
+//layer 5 hat
+import predator_honeyCell_5 from "./掠夺阵营/5-蜂窝帽_sm.png";
+import predator_ironPot_5 from "./掠夺阵营/5-铁锅帽_sm.png";
+import predator_areb_crown_5 from "./掠夺阵营/5-Areb-crown_sm.png";
+//layer 6 glasses
+import predator_triangle_6 from "./掠夺阵营/6-三角眼镜_sm.png";
+import predator_white_6 from "./掠夺阵营/6-白框眼镜_sm.png";
+import predator_roundGlasses_6 from "./掠夺阵营/6-圆框眼镜_sm.png";
+import predator_largeSunGlasses_6 from "./掠夺阵营/6-描边墨镜_sm.png";
+//layer 7 smoke
+import predator_cigarette_7 from "./掠夺阵营/7-卷烟_sm.png";
+import predator_pipe_7 from "./掠夺阵营/7-烟斗_sm.png";
+import predator_cigar_7 from "./掠夺阵营/7-雪茄_sm.png";
+//layer 8 weapon
+import predator_pan_8 from "./掠夺阵营/8b-平底锅_sm.png";
+import predator_chainSaw_8 from "./掠夺阵营/8b-电锯_sm.png";
+import predator_ironShove_8 from "./掠夺阵营/8b-铁铲_sm.png";
+import predator_sickle_8 from "./掠夺阵营/8b-镰刀_sm.png";
+import predator_bullas_whip_8 from "./掠夺阵营/8-BullasWhip_sm.png";
+//layer 9 nail
+import predator_topNail_9 from "./掠夺阵营/9-上层爪子_sm.png";
+
+//-------------pot-------------
+//layer 0 bgimage for pot
+import pot_farm_0 from "./蜜罐阵营/0-农场-compressed_sm.png";
+import pot_house_0 from "./蜜罐阵营/0-家庭_sm.png";
+import pot_aigen_0 from "./蜜罐阵营/0-AIgen-compressed_sm.png";
+import pot_beraboyz_dollarbills_0 from "./蜜罐阵营/0-BeraBoyz-DollarBills_sm.png";
+import pot_beraboyz_paltrees_0 from "./蜜罐阵营/0-BeraBoyz-PalmTrees_sm.png";
+import pot_smileeBeras_bg_0 from "./蜜罐阵营/0-SmileeBeras-bg_sm.png";
+//layer 1 bear type
+import pot_polarBear_1 from "./蜜罐阵营/1-北极熊_sm.png";
+import pot_brownBear_1 from "./蜜罐阵营/1-棕熊_sm.png";
+import pot_pandaBear_1 from "./蜜罐阵营/1-熊猫_sm.png";
+import pot_blackBear_1 from "./蜜罐阵营/1-黑熊_sm.png";
+import pot_beraboyz_blackBear_1 from "./蜜罐阵营/1-Beraboyz黑熊_sm.png";
+import pot_beraboyz_pandaBear_1 from "./蜜罐阵营/1-Beraboyz熊猫_sm.png";
+import pot_beraboyz_polarBear_1 from "./蜜罐阵营/1-Beraboyz北极熊_sm.png";
+import pot_beraboyz_brownBear_1 from "./蜜罐阵营/1-Beraboyz棕熊_sm.png";
+import pot_berabaddies_blackBear_1 from "./蜜罐阵营/1-BeraBaddies_black_sm.png";
+import pot_berabaddies_pandaBear_1 from "./蜜罐阵营/1-BeraBaddies_panda_sm.png";
+import pot_berabaddies_polarBear_1 from "./蜜罐阵营/1-BeraBaddies_polar_sm.png";
+import pot_berabaddies_brownBear_1 from "./蜜罐阵营/1-BeraBaddies_brown_sm.png";
+import pot_Berautistics_blackBear_1 from "./蜜罐阵营/1-Berautistics-black_sm.png";
+import pot_Berautistics_pandaBear_1 from "./蜜罐阵营/1-Berautistics-panda_sm.png";
+import pot_Berautistics_polarBear_1 from "./蜜罐阵营/1-Berautistics-polar_sm.png";
+import pot_Berautistics_brownBear_1 from "./蜜罐阵营/1-Berautistics-brown_sm.png";
+import pot_LasBeras_blackBear_1 from "./蜜罐阵营/1-LasBeras-black_sm.png";
+import pot_LasBeras_pandaBear_1 from "./蜜罐阵营/1-LasBeras-panda_sm.png";
+import pot_LasBeras_polarBear_1 from "./蜜罐阵营/1-LasBeras-polar_sm.png";
+import pot_LasBeras_brownBear_1 from "./蜜罐阵营/1-LasBeras-brown_sm.png";
+import pot_THC_blackBear_1 from "./蜜罐阵营/1-THC黑熊_sm.png";
+import pot_THC_pandaBear_1 from "./蜜罐阵营/1-THC熊猫_sm.png";
+import pot_THC_polarBear_1 from "./蜜罐阵营/1-THC北极熊_sm.png";
+import pot_THC_brownBear_1 from "./蜜罐阵营/1-THC棕熊_sm.png";
+import pot_burrbear_blackBear_1 from "./蜜罐阵营/1-burrbear-black_sm.png";
+import pot_burrbear_pandaBear_1 from "./蜜罐阵营/1-burrbear-panda_sm.png";
+import pot_burrbear_polarBear_1 from "./蜜罐阵营/1-burrbear-white_sm.png";
+import pot_burrbear_brownBear_1 from "./蜜罐阵营/1-burrbear-brown_sm.png";
+import pot_smileeBeras_blackBear_1 from "./蜜罐阵营/1-SmileeBeras-black_sm.png";
+import pot_smileeBeras_pandaBear_1 from "./蜜罐阵营/1-SmileeBeras-panda_sm.png";
+import pot_smileeBeras_polarBear_1 from "./蜜罐阵营/1-SmileeBeras-polar_sm.png";
+import pot_smileeBeras_brownBear_1 from "./蜜罐阵营/1-SmileeBeras-brown_sm.png";
+
+//layer 2 nail
+//import pot_bottomNail_2 from "./蜜罐阵营/2-下层爪子_sm.png";
+//layer 3 emotion
+import pot_anger_3 from "./蜜罐阵营/3-生气_sm.png";
+import pot_normal_3 from "./蜜罐阵营/3-默认_sm.png";
+import pot_nervous_3 from "./蜜罐阵营/3-紧张_sm.png";
+import pot_beraboyz_stare_3 from "./蜜罐阵营/3-Beraboyz瞪眼_sm.png";
+import pot_berabaddies_face_3 from "./蜜罐阵营/3-berabaddies_face_sm.png";
+import pot_BeraPong_face_3 from "./蜜罐阵营/3-BeraPong-face_sm.png";
+import pot_burrbear_face_3 from "./蜜罐阵营/3-burrbear-face_sm.png";
+
+//layer 4 clothes
+import pot_purple_4 from "./蜜罐阵营/4-紫衣服_sm.png";
+import pot_green_4 from "./蜜罐阵营/4-绿衣服_sm.png";
+import pot_beraboyz_golden_pendant_4 from "./蜜罐阵营/4-Beraboyz金吊坠_sm.png";
+import pot_fable_cape_4 from "./蜜罐阵营/4-Fable紫色长袍_sm.png";
+import pot_thc_garland_4 from "./蜜罐阵营/4-THC花环_sm.png";
+import pot_BeraPong_necklace_4 from "./蜜罐阵营/4-BeraPong-necklace_sm.png";
+import pot_berahorse_cloth_4 from "./蜜罐阵营/4-BeraHorse-cloth_sm.png";
+import pot_burrbear_cloth_4 from "./蜜罐阵营/4-burrbear-cloth_sm.png";
+import pot_smileeBeras_dick_4 from "./蜜罐阵营/4-SmileeBeras-dick_sm.png";
+
+//layer 5 hat
+import pot_red_5 from "./蜜罐阵营/5-红帽子_sm.png";
+import pot_honeyCellHat_5 from "./蜜罐阵营/5-蜂窝帽_sm.png";
+import pot_ironPot_5 from "./蜜罐阵营/5-铁锅帽_sm.png";
+import pot_berabaddies_flower_5 from "./蜜罐阵营/5-BeraBaddies_flower_sm.png";
+import pot_berabaddies_hairband_5 from "./蜜罐阵营/5-BeraBaddies_hairband_sm.png";
+import pot_berahourse_5 from "./蜜罐阵营/5-BeraHorse_sm.png";
+import pot_Beracer_helmet_5 from "./蜜罐阵营/5-Beracer头盔_sm.png";
+import pot_smileebera_dickhead_5 from "./蜜罐阵营/5-SmileeBeras-dickhead_sm.png";
+
+//layer 6 glasses
+import pot_triangle_6 from "./蜜罐阵营/6-三角眼镜_sm.png";
+import pot_iron_6 from "./蜜罐阵营/6-金属框墨镜_sm.png";
+import pot_roundGlasses_6 from "./蜜罐阵营/6-圆框眼镜_sm.png";
+import pot_largeSunGlasses_6 from "./蜜罐阵营/6-描边墨镜_sm.png";
+import pot_thc_glasses_6 from "./蜜罐阵营/6-THC眼镜_sm.png";
+import pot_Berautistics_glasses_6 from "./蜜罐阵营/6-Berautistics-glasses_sm.png";
+import pot_burrbear_glasses_6 from "./蜜罐阵营/6-burrbear-glasses_sm.png";
+//layer 7 smoke
+import pot_cigarette_7 from "./蜜罐阵营/7-卷烟_sm.png";
+import pot_pipe_7 from "./蜜罐阵营/7-烟斗_sm.png";
+import pot_cigar_7 from "./蜜罐阵营/7-雪茄_sm.png";
+import pot_smileeberas_lolipop_7 from "./蜜罐阵营/7-SmileeBeras-lolipop_sm.png";
+//layer 8 weapon
+import pot_ikeaBag_8 from "./蜜罐阵营/8a-IKEA购物袋_sm.png";
+import pot_honeyCell_8 from "./蜜罐阵营/8a-蜂窝_sm.png";
+import pot_ironPot_8 from "./蜜罐阵营/8a-铁锅_sm.png";
+import pot_clayPot_8 from "./蜜罐阵营/8a-陶罐_sm.png";
+import pot_bronzeHotPot_8 from "./蜜罐阵营/8a-铜火锅_sm.png";
+import pot_highPressurePot_8 from "./蜜罐阵营/8a-高压锅_sm.png";
+import pot_thj_doll_8 from "./蜜罐阵营/8-THJ玩偶_sm.png";
+import pot_yeetie_doll_8 from "./蜜罐阵营/8-Yeetie玩偶_sm.png";
+import pot_THC_black_arm_8 from "./蜜罐阵营/8-THC黑熊左臂_sm.png";
+import pot_THC_panda_arm_8 from "./蜜罐阵营/8-THC熊猫左臂_sm.png";
+import pot_THC_polar_arm_8 from "./蜜罐阵营/8-THC北极熊左臂_sm.png";
+import pot_THC_brown_arm_8 from "./蜜罐阵营/8-THC棕熊左臂_sm.png";
+import pot_berahorse_rope_8 from "./蜜罐阵营/8-BeraHorse-rope_sm.png";
+import pot_boogaberas_doll_8 from "./蜜罐阵营/8-BoogaBeras-doll_sm.png";
+
+// Bullas	predator *1	Bullas *4	Farm/jail *2	anger/normal *2	green/purple *2	none *1	none	none/cigarette *2	wipe *1	64
+// Beraborrow	predator *1	Beraborrow *4	Farm/jail *2	Beraborrow *1	none	none/ironpot	none/white	Cigar/pipe *2	wipe *1	64
+
+// BeraBoy	pot *1	BeraBoy *4	Beraboy*2	BeraBoy *1	Beraboy *1	none/redhat/Beracer *3	none	Cigar/pipe *2	none	48
+// BeraBaddies	pot *1	BeraBaddies *4	Beraboy tree/farm *2	BeraBaddies *1	THC *1	BeraBaddies/redhat *3	none/Berautistics *2	none	none	48
+// Berautistics	pot *1	Berautistics *4	house *1	none	none/BurrBera *2	none	Berautistics *1	none	Ibeabag/THJ/boogabera/yeet dolls *4	32
+// THC	pot *1	THC *4	Beraboy/house *3	normal*1	thc *1	none/redhat/beracer *3	THC *1	none	THC arm *1(for each bear)	36
+// BurrBera	pot *1	BurrBera *4	Beraboy *2	BurrBera *1	BurrBera *1	none	BurrBera *1	Cigar/pipe *2	none	16
+// Smilee	pot *1	Smilee *4	Smilee/house *2	none	Smilee *1	Smilee *1	none	Smilee *1	rope/None/Honeycell/THJ/boogabera/yeet dolls *6	48
+// BeraBaddies X Fable	pot *1	BeraBaddies *4	farm *1	BeraBaddies *1	fable *1	BeraBaddies *2	none/Berautistics *2	none	none	16
+
+// POT X Areb	predator *1	normal *4	Farm/jail *2	anger/normal *2	green/purple *2	areb	none	none	wipe *1	32
+// POT predator	predator *1	normal *4	Farm/jail/aigen *3	anger/normal *2	green/purple *2	none/cell/ironpot *3	none/white *2	all *3	pan/saw/shove/sickle *4	1728
+
+// POT X Fable	pot *1	normal *4	farm *1	normal*1	fable *1	none	none	Cigar/pipe *2	none/clay/THJ/boogabera/yeet dolls *5	80
+// POT X Beracer	pot *1	normal *4	farm *1	normal*1	green/burrbera/purple *3	Beracer	none	none	none	15
+// POT pot	pot *1	normal *4	house/farm/aigen*3	normala+(anger/nervous 随机二选一) *2	none/green/purple *3	redhat/ironpot/none *3	iron/large/none *3	none/cigarette *2	cell/hotpot/highpresure*3	3888
+
+const generate_setting = {
+  //predators
+  Bullas: {
+    0: [
+      { name: "Farm", img: predator_farm_0 },
+      { name: "Jail", img: predator_jail_0 },
+    ],
+    1: [
+      { name: "Bullas Polar Bear", img: predator_bullas_polarBear_1 },
+      { name: "Bullas Brown Bear", img: predator_bullas_brownBear_1 },
+      { name: "Bullas Panda Bear", img: predator_bullas_pandaBear_1 },
+      { name: "Bullas Black Bear", img: predator_bullas_blackBear_1 },
+    ],
+    2: [{ name: "Bottom Nail", img: predator_bottomNail_2 }],
+    3: [
+      { name: "Anger", img: predator_anger_3 },
+      { name: "Normal", img: predator_normal_3 },
+    ],
+    4: [
+      { name: "Purple", img: predator_purple_4 },
+      { name: "Green", img: predator_green_4 },
+    ],
+    5: [{ name: "None", img: none }],
+    6: [{ name: "None", img: none }],
+    7: [
+      { name: "None", img: none },
+      { name: "Cigarette", img: predator_cigarette_7 },
+    ],
+    8: [{ name: "Bullas Whip", img: predator_bullas_whip_8 }],
+  },
+  Beraborrow: {
+    0: [
+      { name: "Farm", img: predator_farm_0 },
+      { name: "Jail", img: predator_jail_0 },
+    ],
+    1: [
+      { name: "Beraborrow Polar Bear", img: predator_beraborrow_polarBear_1 },
+      { name: "Beraborrow Brown Bear", img: predator_beraborrow_brownBear_1 },
+      { name: "Beraborrow Panda Bear", img: predator_beraborrow_pandaBear_1 },
+      { name: "Beraborrow Black Bear", img: predator_beraborrow_blackBear_1 },
+    ],
+    2: [{ name: "Bottom Nail", img: predator_bottomNail_2 }],
+    3: [{ name: "Beraborrow Smirk", img: predator_beraborrow_smirk_3 }],
+    4: [{ name: "None", img: none }],
+    5: [
+      { name: "None", img: none },
+      { name: "Iron Pot", img: predator_ironPot_5 },
+    ],
+    6: [
+      { name: "None", img: none },
+      { name: "White", img: predator_white_6 },
+    ],
+    7: [
+      { name: "Pipe", img: predator_pipe_7 },
+      { name: "Cigar", img: predator_cigar_7 },
+    ],
+    8: [{ name: "Iron Shove", img: predator_bullas_whip_8 }],
+  },
+  Areb: {
+    0: [
+      { name: "Farm", img: predator_farm_0 },
+      { name: "Jail", img: predator_jail_0 },
+    ],
+    1: [
+      { name: "Polar Bear", img: predator_polarBear_1 },
+      { name: "Brown Bear", img: predator_brownBear_1 },
+      { name: "Panda Bear", img: predator_pandaBear_1 },
+      { name: "Black Bear", img: predator_blackBear_1 },
+    ],
+    2: [{ name: "Bottom Nail", img: predator_bottomNail_2 }],
+    3: [{ name: "Areb Face", img: predator_areb_face_3 }],
+    4: [
+      { name: "Green", img: predator_green_4 },
+      { name: "Purple", img: predator_purple_4 },
+    ],
+    5: [{ name: "Areb Crown", img: predator_areb_crown_5 }],
+    6: [{ name: "None", img: none }],
+    7: [{ name: "None", img: none }],
+    8: [{ name: "Iron Shove", img: predator_bullas_whip_8 }],
+  },
+  HPOT_Predator: {
+    0: [
+      { name: "Farm", img: predator_farm_0 },
+      { name: "Jail", img: predator_jail_0 },
+      { name: "AI Gen", img: pot_aigen_0 },
+    ],
+    1: [
+      { name: "Polar Bear", img: predator_polarBear_1 },
+      { name: "Brown Bear", img: predator_brownBear_1 },
+      { name: "Panda Bear", img: predator_pandaBear_1 },
+      { name: "Black Bear", img: predator_blackBear_1 },
+    ],
+    2: [{ name: "Bottom Nail", img: predator_bottomNail_2 }],
+    3: [
+      { name: "Normal", img: predator_normal_3 },
+      { name: "Anger", img: predator_anger_3 },
+    ],
+    4: [
+      { name: "Green", img: predator_green_4 },
+      { name: "Purple", img: predator_purple_4 },
+    ],
+    5: [
+      { name: "None", img: none },
+      { name: "Honey Cell", img: predator_honeyCell_5 },
+      { name: "Iron Pot", img: predator_ironPot_5 },
+    ],
+    6: [
+      { name: "None", img: none },
+      { name: "White", img: predator_white_6 },
+    ],
+    7: [
+      { name: "Cigarette", img: predator_cigarette_7 },
+      { name: "Pipe", img: predator_pipe_7 },
+      { name: "Cigar", img: predator_cigar_7 },
+    ],
+    8: [
+      { name: "Pan", img: predator_pan_8 },
+      { name: "Chain Saw", img: predator_chainSaw_8 },
+      { name: "Iron Shove", img: predator_ironShove_8 },
+      { name: "Sickle", img: predator_sickle_8 },
+    ],
+    9: [{ name: "Top Nail", img: predator_topNail_9 }],
+  },
+  //pots
+  BeraBoyz: {
+    0: [
+      { name: "Dollar Bills", img: pot_beraboyz_dollarbills_0 },
+      { name: "Pal Trees", img: pot_beraboyz_paltrees_0 },
+    ],
+    1: [
+      { name: "Polar Bear", img: pot_beraboyz_polarBear_1 },
+      { name: "Brown Bear", img: pot_beraboyz_brownBear_1 },
+      { name: "Panda Bear", img: pot_beraboyz_pandaBear_1 },
+      { name: "Black Bear", img: pot_beraboyz_blackBear_1 },
+    ],
+    2: [{ name: "None", img: none }],
+    3: [{ name: "BeraBoyz Stare", img: pot_beraboyz_stare_3 }],
+    4: [{ name: "Golden Pendant", img: pot_beraboyz_golden_pendant_4 }],
+    5: [
+      { name: "None", img: none },
+      { name: "Red Hat", img: pot_red_5 },
+      { name: "Beracer Helmet", img: pot_Beracer_helmet_5 },
+    ],
+    6: [{ name: "None", img: none }],
+    7: [
+      //cigar
+      { name: "Cigar", img: pot_cigar_7 },
+      //pipe
+      { name: "Pipe", img: pot_pipe_7 },
+    ],
+    8: [{ name: "None", img: none }],
+    9: [{ name: "None", img: none }],
+  },
+  BeraBaddies: {
+    0: [
+      { name: "Pal Trees", img: pot_beraboyz_paltrees_0 },
+      { name: "Farm", img: predator_farm_0 },
+    ],
+    1: [
+      { name: "Black Bear", img: pot_berabaddies_blackBear_1 },
+      { name: "Panda Bear", img: pot_berabaddies_pandaBear_1 },
+      { name: "Polar Bear", img: pot_berabaddies_polarBear_1 },
+      { name: "Brown Bear", img: pot_berabaddies_brownBear_1 },
+    ],
+    2: [
+      { name: "Tree", img: pot_berabaddies_flower_5 },
+      { name: "Farm", img: predator_farm_0 },
+    ],
+    3: [{ name: "Berabaddies Face", img: pot_berabaddies_face_3 }],
+    4: [{ name: "THC", img: pot_thc_garland_4 }],
+    5: [
+      { name: "None", img: none },
+      { name: "Red Hat", img: pot_berabaddies_hairband_5 },
+    ],
+    6: [
+      { name: "None", img: none },
+      { name: "Berautistics Glasses", img: pot_Berautistics_glasses_6 },
+    ],
+    7: [{ name: "None", img: none }],
+    8: [{ name: "None", img: none }],
+  },
+};
